70cef6a9c38c9af8b076e8af43d0714e55e74559
Alexcooler <alexcooler060296@gmail.com>
2021-04-23 18:18:52 +0300

    правки

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-const modalclose = document.querySelector('.modal-close');
+const modalclose = document.querySelector(".modal-close");
 const modalfeedback = document.querySelector(".modal-feedback");
 const feedbackbutton = document.querySelector(".feedback-button");
 const assortment = document.querySelector(".modal-assortment");
@@ -9,55 +8,53 @@ const search = document.querySelector(".search");
 const input = document.querySelector(".input");
 const popup = document.querySelector(".modal-login");
 const login = popup.querySelector("[name=login]");
-const buttonFirstSlide = document.querySelector('.first-slide-btn');
-const buttonSecondSlide = document.querySelector('.second-slide-btn');
-const buttonThirdSlide = document.querySelector('.third-slide-btn');
-const firstSlide = document.querySelector('.first-slide');
-const secondSlide = document.querySelector('.second-slide');
-const thirdSlide = document.querySelector('.third-slide');
-const background = document.querySelector('.site-wrapper');
-
+const buttonFirstSlide = document.querySelector(".first-slide-btn");
+const buttonSecondSlide = document.querySelector(".second-slide-btn");
+const buttonThirdSlide = document.querySelector(".third-slide-btn");
+const firstSlide = document.querySelector(".first-slide");
+const secondSlide = document.querySelector(".second-slide");
+const thirdSlide = document.querySelector(".third-slide");
+const background = document.querySelector(".site-wrapper");
 
 /* SLIDER */
-buttonFirstSlide.addEventListener('click', function(evt) {
+buttonFirstSlide.addEventListener("click", function (evt) {
   evt.preventDefault();
-  buttonFirstSlide.classList.toggle('current');
-  buttonSecondSlide.classList.remove('current');
-  buttonThirdSlide.classList.remove('current');
-  firstSlide.classList.toggle('slide-current');
-  secondSlide.classList.remove('slide-current');
-  thirdSlide.classList.remove('slide-current');
-  background.classList.toggle('site-wrapper-1');
-  background.classList.remove('site-wrapper-2');
-  background.classList.remove('site-wrapper-3');
+  buttonFirstSlide.classList.toggle("current");
+  buttonSecondSlide.classList.remove("current");
+  buttonThirdSlide.classList.remove("current");
+  firstSlide.classList.toggle("slide-current");
+  secondSlide.classList.remove("slide-current");
+  thirdSlide.classList.remove("slide-current");
+  background.classList.toggle("site-wrapper-1");
+  background.classList.remove("site-wrapper-2");
+  background.classList.remove("site-wrapper-3");
 });
 
-buttonSecondSlide.addEventListener('click', function(evt) {
+buttonSecondSlide.addEventListener("click", function (evt) {
   evt.preventDefault();
-  buttonSecondSlide.classList.toggle('current');
-  buttonFirstSlide.classList.remove('current');
-  buttonThirdSlide.classList.remove('current');
-  secondSlide.classList.toggle('slide-current');
-  firstSlide.classList.remove('slide-current');
-  thirdSlide.classList.remove('slide-current');
-  background.classList.toggle('site-wrapper-2');
-  background.classList.remove('site-wrapper-1');
-  background.classList.remove('site-wrapper-3');
+  buttonSecondSlide.classList.toggle("current");
+  buttonFirstSlide.classList.remove("current");
+  buttonThirdSlide.classList.remove("current");
+  secondSlide.classList.toggle("slide-current");
+  firstSlide.classList.remove("slide-current");
+  thirdSlide.classList.remove("slide-current");
+  background.classList.toggle("site-wrapper-2");
+  background.classList.remove("site-wrapper-1");
+  background.classList.remove("site-wrapper-3");
 });
 
-buttonThirdSlide.addEventListener('click', function(evt) {
+buttonThirdSlide.addEventListener("click", function (evt) {
   evt.preventDefault();
-  buttonThirdSlide.classList.toggle('current');
-  buttonFirstSlide.classList.remove('current');
-  buttonSecondSlide.classList.remove('current');
-  thirdSlide.classList.toggle('slide-current');
-  firstSlide.classList.remove('slide-current');
-  secondSlide.classList.remove('slide-current');
-  background.classList.toggle('site-wrapper-3');
-  background.classList.remove('site-wrapper-2');
-  background.classList.remove('site-wrapper-1');
+  buttonThirdSlide.classList.toggle("current");
+  buttonFirstSlide.classList.remove("current");
+  buttonSecondSlide.classList.remove("current");
+  thirdSlide.classList.toggle("slide-current");
+  firstSlide.classList.remove("slide-current");
+  secondSlide.classList.remove("slide-current");
+  background.classList.toggle("site-wrapper-3");
+  background.classList.remove("site-wrapper-2");
+  background.classList.remove("site-wrapper-1");
 });
-
 
 /* MODAL */
 input.addEventListener("click", function (evt) {
@@ -82,7 +79,7 @@ feedbackbutton.addEventListener("click", function (evt) {
   modalfeedback.classList.toggle("modal-show");
 });
 
-modalclose.addEventListener('click', function(evt) {
+modalclose.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modalfeedback.classList.remove('modal-show');
+  modalfeedback.classList.remove("modal-show");
 });
